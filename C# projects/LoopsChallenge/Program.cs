ab71ce50ae75371f2073c8193881b64208e46a62
Fasoft <101709684+FARAMATSI@users.noreply.github.com>
2023-03-24 14:35:37 +0200

    loop challenge

---
+++ C# projects/LoopsChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LoopsChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 6; i >=2; i-=2)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
