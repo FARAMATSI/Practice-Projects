5dd764d2562d0f4a0f445d355fc48bf9a9179d0d
Fasoft <101709684+FARAMATSI@users.noreply.github.com>
2023-03-24 02:03:43 +0200

    mass ommit

---
+++ C# projects/Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 2;
+            Console.WriteLine("Hello, World!",a+b);
+        }
+    }
+}
