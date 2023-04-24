7ed2a774b36fd69784b6336a90a7b6168be8f058
Fasoft <101709684+FARAMATSI@users.noreply.github.com>
2023-04-24 19:12:52 +0200

    The C# journey, Practice makes perfect

---
+++ C# projects/C# Practice qn 10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Transactions;
+namespace C__Practice_qn_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[5];
+            int[] secondArray = new int[5];
+            Console.WriteLine("\t Enter the integer array elements for the First array");
+            int maxIndex = 5;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.WriteLine("Enter Element on index " + i);
+                firstArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // Filling the second array
+            Console.WriteLine();
+            Console.WriteLine("\t Enter the integer array elements for the Second Array");
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                Console.WriteLine("Enter Element on index " + i);
+                secondArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // Displaying the first array
+            Console.Write("First Array elements are : \t");
+            for (int i = 0; i < firstArray.Length; i++) {
+                Console.Write(firstArray[i] + ", ");
+            }
+            Console.WriteLine();
+            // Displaying the Second array
+            Console.Write("Second Array elements are : \t");
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                Console.Write(secondArray[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            // Perfoming the addition now 
+            for (int i =0; i < firstArray.Length; i++)
+            {
+                firstArray[i]+= secondArray[i];
+            }
+            Console.Write("\t \t The RESULTING Array is : ");
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.Write(firstArray[i]+",");
+            }
+        }
+    }
+}
